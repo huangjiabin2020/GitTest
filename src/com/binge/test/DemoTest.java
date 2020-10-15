5c92889339c199c9237dd1a08f8b81856fd66e1a
haungjaibin2020 <826819173@qq.com>
2020-10-15 19:36:33 +0800

    第一次提交

---
+++ src/com/binge/test/DemoTest.java
@@ -0,0 +1,11 @@
+package com.binge.test;
+
+/**
+ * @author JiaBin Huang
+ * @date 2020/10/15
+ **/
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("666");
+    }
+}
